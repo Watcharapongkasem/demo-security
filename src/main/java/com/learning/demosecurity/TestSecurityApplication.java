0138af0350dc4764b7dede3ffcaf7f7c84228c73
MSI\make_ <watcharapong.kasemsat@apar.com>
2023-11-29 21:33:11 +0700

    init

---
+++ src/main/java/com/learning/demosecurity/TestSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.learning.demosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/demosecurity/TestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
